3ab50faebc5ec41b25c68b378cd88036710fd5ec
DmitryAstafyev <DVAstafyev@gmail.com>
2015-06-11 12:31:28 +0200

    C#

---
+++ Kernel/C#NET/ModulesController.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+using System.Text;
+using System.Web.Script.Serialization;
+using Purity.Models;
+using Purity.Classes;
+
+namespace Purity.Controllers
+{
+    /*
+     * Данный контролер отвечает за "выдачу" модулей.
+     * Здесь живет только один запрос - get, доступный по адресу http://[domain]/modules/get.
+     * Данный запрос возвращает данные модулей, либо сообщение об ошибке, если модуль не найден.
+     */
+    public class ModulesController : Controller
+    {
+        //Класс, отвечающий за доставку "тела" модуля
+        public class getContentClass
+        {
+            string PhysicalApplicationPath;
+            string UrlScheme;
+            string UrlAuthority;
+            public getContentClass(string PhysicalApplicationPath, string UrlScheme, string UrlAuthority)
+            {
+                this.PhysicalApplicationPath    = PhysicalApplicationPath;
+                this.UrlScheme                  = UrlScheme;
+                this.UrlAuthority               = UrlAuthority;
+            }
+            public List<Dictionary<string, string>> get(string[] modulesNames)
+            {
+                List<Dictionary<string, string>>    modulesContent  = new List<Dictionary<string, string>>();
+                Dictionary<string, string>          moduleRecord;
+                FileStream                          fileStream;
+                StreamReader                        streamReader;
+                FileInfo                            fileInfo;
+                string                              PathToFile;
+                string                              fileName;
+                string                              URLToFile;
+                string                              ResultString;
+                string                              moduleName;
+                byte[]                              bytes;
+                for (int Index = modulesNames.Length - 1; Index >= 0; Index -= 1)
+                {
+                    fileName        = (modulesNames[Index].ToLower().IndexOf("purity.") != -1 ? modulesNames[Index] + ".js" : "Purity." + modulesNames[Index] + ".js");
+                    moduleName      = (modulesNames[Index].ToLower().IndexOf("purity.") == -1 ? modulesNames[Index] :  modulesNames[Index].Substring("Purity.".Length, modulesNames[Index].Length - "Purity.".Length)); 
+                    PathToFile      = this.PhysicalApplicationPath + @"Kernel\JS\" + fileName;
+                    URLToFile       = UrlScheme + "://" + UrlAuthority + "/" + @"Kernel/JS/" + fileName;
+                    moduleRecord    = new Dictionary<string, string>();
+                    if (System.IO.File.Exists(PathToFile) == true)
+                    {
+                        fileInfo = new FileInfo(PathToFile);
+                        if (fileInfo.Length > 0)
+                        {
+                            fileStream      = new FileStream(PathToFile, FileMode.Open, FileAccess.Read);
+                            streamReader    = new StreamReader(fileStream);
+                            ResultString    = streamReader.ReadToEnd();
+                            fileStream.Close();
+                            bytes           = System.Text.UTF8Encoding.UTF8.GetBytes(ResultString);
+                            //Записываем данные
+                            moduleRecord.Add("value",   Convert.ToBase64String(bytes));
+                            moduleRecord.Add("name",    moduleName);
+                            moduleRecord.Add("version", fileInfo.LastWriteTime.ToString());
+                            moduleRecord.Add("url",     URLToFile);
+                        }
+                    }
+                    if (moduleRecord.Keys.Count == 0){
+                        moduleRecord.Add("value",   "null");
+                        moduleRecord.Add("name",    moduleName);
+                        moduleRecord.Add("version", "null");
+                        moduleRecord.Add("url",     "null");
+                    }
+                    modulesContent.Add(moduleRecord);
+                }
+                return modulesContent;
+            }
+            public List<Dictionary<string, string>> list()
+            {
+                List<Dictionary<string, string>>    resourcesList  = new List<Dictionary<string, string>>();
+                Dictionary<string, string>          resourceRecord;
+                FileInfo                            fileInfo;
+                string                              PathToDirectory = PhysicalApplicationPath + @"Kernel\JS\";
+                string                              fileName;
+                string                              moduleName;
+                string[]                            allResources;
+                if (System.IO.Directory.Exists(PathToDirectory) == true)
+                {
+                    allResources = System.IO.Directory.GetFiles(PathToDirectory,"*.*", SearchOption.AllDirectories);
+                    for (int index = allResources.Length - 1; index >= 0; index -= 1)
+                    {
+                        fileInfo = new FileInfo(allResources[index]);
+                        if (fileInfo.Length > 0)
+                        {
+                            fileName        = fileInfo.Name;
+                            moduleName      = (fileName.ToLower().IndexOf("purity.") == -1 ? fileName : fileName.Substring("Purity.".Length, fileName.Length - "Purity.".Length));
+                            moduleName      = (moduleName.ToLower().IndexOf(".js") == -1 ? moduleName : moduleName.Substring(0, moduleName.Length - ".js".Length));
+                            resourceRecord = new Dictionary<string, string>();
+                            resourceRecord.Add("version",   fileInfo.LastWriteTime.ToString());
+                            resourceRecord.Add("name",      moduleName);
+                            resourcesList.Add(resourceRecord);
+                        }
+                    }
+                }
+                return resourcesList;
+            }
+        }
+        //Обработка запроса
+        //[HttpGet]
+        public JsonResult get(requestModulesClass requestModules)
+        {
+            try {
+                string[]        modulesNames    = (requestModules.modules != null ? requestModules.modules.Split(';') : null);
+                getContentClass getContent      = new getContentClass(  HttpContext.Request.PhysicalApplicationPath,
+                                                                        HttpContext.Request.Url.Scheme,
+                                                                        HttpContext.Request.Url.Authority);
+                JsonResult      jsonResult;
+                switch (requestModules.command)
+                {
+                    case "content":
+                        jsonResult                  = Json(getContent.get(modulesNames), JsonRequestBehavior.AllowGet);
+                        jsonResult.MaxJsonLength    = int.MaxValue;
+                        if (requestModules.JSIC == null)
+                        {
+                            return jsonResult;
+                        }
+                        else
+                        {
+                            return Json("Purity server's script::: Incorrect request", JsonRequestBehavior.AllowGet);
+                        }
+                    case "urls only":
+
+                        break;
+                    case "versions":
+                        jsonResult                  = Json(getContent.list(), JsonRequestBehavior.AllowGet);
+                        jsonResult.MaxJsonLength    = int.MaxValue;
+                        if (requestModules.JSIC == null)
+                        {
+                            return jsonResult;
+                        }
+                        else
+                        {
+                            return Json("Purity server's script::: Incorrect request", JsonRequestBehavior.AllowGet);
+                        }
+                        break;
+                }
+                return Json("Purity server's script::: Incorrect command", JsonRequestBehavior.AllowGet);
+            }
+            catch (System.Exception ex)
+            {
+                return Json("Purity server's script::: Some error on server side", JsonRequestBehavior.AllowGet);
+            }
+        }
+        
+        public string getJSIC(requestModulesClass requestModules)
+        {
+            try
+            {
+                string[]        modulesNames    = (requestModules.modules != null ? requestModules.modules.Split(';') : null);
+                string          JSICString      = "";
+                getContentClass getContent      = new getContentClass(HttpContext.Request.PhysicalApplicationPath,
+                                                                    HttpContext.Request.Url.Scheme,
+                                                                    HttpContext.Request.Url.Authority);
+                JsonResult      jsonResult;
+                switch (requestModules.command)
+                {
+                    case "content":
+                        jsonResult                  = Json(getContent.get(modulesNames), JsonRequestBehavior.AllowGet);
+                        jsonResult.MaxJsonLength    = int.MaxValue;
+                        if (requestModules.JSIC != null)
+                        {
+                            JSICClass JSIC = new JSICClass(requestModules.JSIC);
+                            if (JSIC.isReady == true)
+                            {
+                                JSICString = JSIC.getData(new JavaScriptSerializer().Serialize(jsonResult.Data));
+                            }
+                            return JSICString;
+                        }
+                        else
+                        {
+                            return "Purity server's script::: Cannot get string";
+                        }
+                    case "urls only":
+
+                        break;
+                    case "versions":
+                        jsonResult                  = Json(getContent.list(), JsonRequestBehavior.AllowGet);
+                        jsonResult.MaxJsonLength    = int.MaxValue;
+                        if (requestModules.JSIC != null)
+                        {
+                            JSICClass JSIC = new JSICClass(requestModules.JSIC);
+                            if (JSIC.isReady == true)
+                            {
+                                JSICString = JSIC.getData(new JavaScriptSerializer().Serialize(jsonResult.Data));
+                            }
+                            return JSICString;
+                        }
+                        else
+                        {
+                            return "Purity server's script::: Cannot get string";
+                        }
+                        break;
+                }
+                return "Purity server's script::: Incorrect command";
+            }
+            catch (System.Exception ex)
+            {
+                return "Purity server's script::: Some error on server side";
+            }
+        }
+
+    }
+}
---
+++ Kernel/C#NET/ResourcesController.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+using Purity.Classes;
+using System.Web.Script.Serialization;
+using System.Text;
+using Purity.Models;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Net.Http.Headers;
+
+namespace Purity.Controllers
+{
+    public class ResourcesController : Controller
+    {
+        public class getStringContentClass
+        {
+            public class versionsClass
+            {
+                private Dictionary<string, string> get()
+                {
+                    if (VirtualResourcesVersionsStorageClass.versions != null)
+                    {
+                        return VirtualResourcesVersionsStorageClass.versions;
+                    }
+                    return new Dictionary<string, string>();
+                }
+                private void update(string PhysicalApplicationPath)
+                {
+                    FileInfo fileInfo;
+                    string fileName;
+                    Dictionary<string, string> cachedData = this.get();
+                    for (var index = cachedData.Keys.Count - 1; index > 0; index -= 1)
+                    {
+                        fileName = cachedData.Keys.ElementAt(index);
+                        fileName = fileName.Replace("[", "");
+                        fileName = fileName.Replace("]", "");
+                        fileName = (PhysicalApplicationPath + fileName).Replace('/', '\\');
+                        if (System.IO.File.Exists(fileName) == true)
+                        {
+                            fileInfo = new FileInfo(fileName);
+                            cachedData[cachedData.Keys.ElementAt(index)] = fileInfo.LastWriteTime.ToString();
+                        }
+                    }
+                    this.set(cachedData);
+                }
+                private void set(Dictionary<string, string> versions)
+                {
+                    VirtualResourcesVersionsStorageClass.versions = versions;
+                }
+                public void registration(string path, string name, string version)
+                {
+                    Dictionary<string, string>  cachedData  = this.get();
+                    string                      resourceKey = path + name;
+                    if (cachedData.ContainsKey(resourceKey) == false)
+                    {
+                        cachedData.Add(resourceKey, version);
+                        this.set(cachedData);
+                    }
+                    else
+                    {
+                        if (cachedData[resourceKey] != version)
+                        {
+                            cachedData[resourceKey] = version;
+                            this.set(cachedData);
+                        }
+                    }
+                }
+                public List<Dictionary<string, string>> list(string PhysicalApplicationPath)
+                {
+                    this.update(PhysicalApplicationPath);
+                    List<Dictionary<string, string>>    resultList  = new List<Dictionary<string, string>>();
+                    Dictionary<string, string>          versions    = this.get();
+                    Dictionary<string, string>          record;
+                    for (var index = versions.Keys.Count - 1; index >= 0; index -= 1)
+                    {
+                        record = new Dictionary<string, string>();
+                        record.Add("resource",  versions.Keys.ElementAt(index)          );
+                        record.Add("version",   versions[versions.Keys.ElementAt(index)]);
+                        resultList.Add(record);
+                    }
+                    return resultList;
+                }
+            }
+            private string          PhysicalApplicationPath;
+            private versionsClass   versions = new versionsClass();
+            public getStringContentClass(string PhysicalApplicationPath)
+            {
+                this.PhysicalApplicationPath    = PhysicalApplicationPath;
+            }
+            public string get(string path, string name)
+            {
+                FileStream                  fileStream;
+                StreamReader                streamReader;
+                FileInfo                    fileInfo;
+                string                      ResultString    = "";
+                string                      FileName;
+                string                      Extension;
+                byte[]                      bytes;
+                Dictionary<string, string>  resourceRecord  = new Dictionary<string,string>();
+                //path = path.Replace("~", "");
+                FileName = (this.PhysicalApplicationPath + path + name).Replace("~", "").Replace('/', '\\').Replace("\\\\", "\\");/*without /*/
+                //FileName                                    = FileName.Replace("~","");
+                if (System.IO.File.Exists(FileName) == true)
+                {
+                    fileInfo = new FileInfo(FileName);
+                    if (fileInfo.Length > 0)
+                    {
+                        Extension       = fileInfo.Extension.ToLower();
+                        //Тут конечно же нужно автоматическое определение текста от бинарных данных
+                        if (Extension.IndexOf("png") == -1 && Extension.IndexOf("jpg") == -1 && Extension.IndexOf("gif") == -1 && Extension.IndexOf("jpeg") == -1)
+                        {
+                            fileStream      = new FileStream(FileName, FileMode.Open, FileAccess.Read);
+                            streamReader    = new StreamReader(fileStream);
+                            ResultString    = streamReader.ReadToEnd();
+                            bytes           = System.Text.UTF8Encoding.UTF8.GetBytes(ResultString);
+                            fileStream.Close();
+                        }
+                        else
+                        {
+                            bytes           = System.IO.File.ReadAllBytes(FileName);
+                        }
+                        //Записываем данные
+                        resourceRecord.Add("value",     Convert.ToBase64String(bytes));
+                        resourceRecord.Add("name",      name);
+                        resourceRecord.Add("version",   fileInfo.LastWriteTime.ToString());
+                        //Регистрируем версию
+                        this.versions.registration(path, name, fileInfo.LastWriteTime.ToString());
+                        return new JavaScriptSerializer().Serialize(resourceRecord);
+                    }
+                }
+                return null;
+            }
+        }
+        //
+        // GET: /Resources/
+        //JSIC (JavaScript Information Communication)
+        
+        [HttpGet]
+        public string get(requestResourseClass requestModules)
+        {
+            if (requestModules.path != null && requestModules.name != null && requestModules.JSIC != null)
+            {
+                getStringContentClass   getStringContent    = new getStringContentClass(HttpContext.Request.PhysicalApplicationPath);
+                string                  JSICString          = getStringContent.get(requestModules.path, requestModules.name);
+                JSICClass               JSIC                = new JSICClass(requestModules.JSIC);
+                JSICString                                  = JSIC.getData(JSICString);
+                if (JSICString != null)
+                {
+                    return JSICString;
+                }
+                
+            }
+            return "JSIC resource request::: (path or name isn't defined in params of request) or (error or server side)";
+        }
+        [HttpGet]
+        public string versions(requestResourseClass requestModules)
+        {
+            try{
+                if (requestModules.JSIC != null)
+                {
+                    getStringContentClass.versionsClass versions = new getStringContentClass.versionsClass();
+                    JsonResult  jsonResult      = Json(versions.list(HttpContext.Request.PhysicalApplicationPath), JsonRequestBehavior.AllowGet);
+                    JSICClass   JSIC            = new JSICClass(requestModules.JSIC);
+                    string      resultString    = JSIC.getData(new JavaScriptSerializer().Serialize(jsonResult.Data));
+                    if (resultString != null)
+                    {
+                        return resultString;
+                    }
+
+                }
+                return "JSIC resource request::: cannot get information about versions";
+            }catch(Exception ex)
+            {
+                return "JSIC resource request::: bad request";
+            }
+        }
+
+    }
+}
